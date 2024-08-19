2b37a40d993a28f7d2d634c7397b159e3fee768d
Jafarscript <lihammedjafar@gmail.com>
2024-08-19 14:27:15 +0100

    second one

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,15 +3,11 @@ const mongoose = require('mongoose');
 const db = async () => {
     try {
         mongoose.set('strictQuery', false);
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            tlsAllowInvalidCertificates: true // Add this line
-        });
+        await mongoose.connect(process.env.MONGO_URL);
         console.log('Db Connected');
     } catch (error) {
         console.log('DB Connection Error:', error.message);
     }
 };
 
-module.exports = { db };
+module.exports = {db}
